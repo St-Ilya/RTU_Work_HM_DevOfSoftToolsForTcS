2e13034bb5c1582f44d0fae0e85452049f05d203
Karpuhin Ilya <karpuhinilya@gmail.com>
2022-12-17 18:10:55 +0300

    delete and add /.idea and /out in .gitignore

--- src/Graphic_Form/App.java
+++ src/Graphic_Form/App.java
@@ -2,6 +2,7 @@ package Graphic_Form;
 
 import javax.swing.*;
 
+//
 public class App {
     public static void main(String[] args) {
         Travel_Tracking_App travel_tracking_app = new Travel_Tracking_App();
